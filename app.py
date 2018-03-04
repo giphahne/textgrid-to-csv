9af44def5ae2e6c07f7d5f491e192943ffef65ab
Dan Hahne <giphahne@gmail.com>
2018-03-04 16:53:16 -0500

    updating python version

--- app.py
+++ app.py
@@ -7,7 +7,8 @@ import urlparse
 
 from dropbox import Dropbox, DropboxOAuth2Flow
 from dropbox.files import DeletedMetadata, FolderMetadata, WriteMode
-from flask import abort, Flask, redirect, render_template, Response, request, session, url_for
+from flask import abort, Flask, redirect, render_template,
+from flask import Response, request, session, url_for
 from markdown import markdown
 import redis
 
@@ -24,29 +25,32 @@ app.debug = True
 # A random secret used by Flask to encrypt session data cookies
 app.secret_key = os.environ['FLASK_SECRET_KEY']
 
+
 def get_url(route):
     '''Generate a proper URL, forcing HTTPS if not running locally'''
     host = urlparse.urlparse(request.url).hostname
     url = url_for(
         route,
         _external=True,
-        _scheme='http' if host in ('127.0.0.1', 'localhost') else 'https'
-    )
+        _scheme='http' if host in ('127.0.0.1', 'localhost') else 'https')
 
     return url
 
+
 def get_flow():
-    return DropboxOAuth2Flow(
-        APP_KEY,
-        APP_SECRET,
-        get_url('oauth_callback'),
-        session,
-        'dropbox-csrf-token')
+    return DropboxOAuth2Flow(APP_KEY, APP_SECRET, get_url('oauth_callback'),
+                             session, 'dropbox-csrf-token')
+
 
 @app.route('/welcome')
 def welcome():
-    return render_template('welcome.html', redirect_url=get_url('oauth_callback'),
-        webhook_url=get_url('webhook'), home_url=get_url('index'), app_key=APP_KEY)
+    return render_template(
+        'welcome.html',
+        redirect_url=get_url('oauth_callback'),
+        webhook_url=get_url('webhook'),
+        home_url=get_url('index'),
+        app_key=APP_KEY)
+
 
 @app.route('/oauth_callback')
 def oauth_callback():
@@ -63,14 +67,19 @@ def oauth_callback():
 
     return redirect(url_for('done'))
 
+
 def process_user(account):
-    '''Call /files/list_folder for the given user ID and process any changes.'''
+    '''
+    Call /files/list_folder for the given user ID and process any changes.
+    '''
 
     # OAuth token for the user
     token = redis_client.hget('tokens', account)
 
     # cursor for the user (None the first time)
     cursor = redis_client.hget('cursors', account)
+
+    file_extension = ".csv"
 
     dbx = Dropbox(token)
     has_more = True
@@ -83,15 +92,18 @@ def process_user(account):
 
         for entry in result.entries:
             # Ignore deleted files, folders, and non-markdown files
-            if (isinstance(entry, DeletedMetadata) or
-                isinstance(entry, FolderMetadata) or
-                not entry.path_lower.endswith('.md')):
+            if (isinstance(entry, DeletedMetadata)
+                    or isinstance(entry, FolderMetadata)
+                    or not entry.path_lower.endswith(file_extension)):
                 continue
 
             # Convert to Markdown and store as <basename>.html
             _, resp = dbx.files_download(entry.path_lower)
             html = markdown(unicode(resp.content, "utf-8")).encode("utf-8")
-            dbx.files_upload(html, entry.path_lower[:-3] + '.html', mode=WriteMode('overwrite'))
+            dbx.files_upload(
+                html,
+                entry.path_lower[:-len(file_extension)] + '.html',
+                mode=WriteMode('overwrite'))
 
         # Update cursor
         cursor = result.cursor
@@ -100,21 +112,28 @@ def process_user(account):
         # Repeat only if there's more to do
         has_more = result.has_more
 
+
 @app.route('/')
 def index():
     return render_template('index.html')
+
 
 @app.route('/login')
 def login():
     return redirect(get_flow().start())
 
+
 @app.route('/done')
 def done():
     return render_template('done.html')
 
+
 @app.route('/webhook', methods=['GET'])
 def challenge():
-    '''Respond to the webhook challenge (GET request) by echoing back the challenge parameter.'''
+    '''
+    Respond to the webhook challenge (GET request) 
+    by echoing back the challenge parameter.
+    '''
 
     resp = Response(request.args.get('challenge'))
     resp.headers['Content-Type'] = 'text/plain'
@@ -122,13 +141,16 @@ def challenge():
 
     return resp
 
+
 @app.route('/webhook', methods=['POST'])
 def webhook():
     '''Receive a list of changed user IDs from Dropbox and process each.'''
 
     # Make sure this is a valid request from Dropbox
     signature = request.headers.get('X-Dropbox-Signature').encode("utf-8")
-    if not hmac.compare_digest(signature, hmac.new(APP_SECRET, request.data, sha256).hexdigest()):
+    if not hmac.compare_digest(signature,
+                               hmac.new(APP_SECRET, request.data,
+                                        sha256).hexdigest()):
         abort(403)
 
     for account in json.loads(request.data)['list_folder']['accounts']:
@@ -136,8 +158,9 @@ def webhook():
         # actual work in a separate thread. For more robustness, it's a
         # good idea to add the work to a reliable queue and process the queue
         # in a worker process.
-        threading.Thread(target=process_user, args=(account,)).start()
+        threading.Thread(target=process_user, args=(account, )).start()
     return ''
 
-if __name__=='__main__':
+
+if __name__ == '__main__':
     app.run(debug=True)
